fd55676d3184dee7991f1a7268556530578d8003
Mateus Huster <mateushuster@gmail.com>
2026-05-05 18:51:07 +0000

    inicio de uma reorganizacao por diferentes metodos

--- clone_grep/src/main.c
+++ clone_grep/src/main.c
@@ -2,42 +2,63 @@
 #include <stdlib.h>
 #include <math.h>
 
+void imprime_resultado(char *linha){
+    printf("%s", linha);
+}
+
+void busca_palavra(char *palavra, char *linha){ //
+    return strstr(linha, palavra);// strstr é uma função da biblioteca string.h que procura a primeira ocorrência de uma substring (neste caso, a palavra) em uma string (neste caso, a linha). Se a substring for encontrada, a função retorna um ponteiro para a primeira ocorrência da substring na string. Caso contrário, retorna NULL.
+}
+
+void imprime_numeroLinha(int numero_linha, char *linha){
+    printf("%d: %s", numero_linha, linha);
+}
+
+
+
+
+
+
+
+
 int main(int argc, char *argv[]) { // argc numero de palavras escritas e *argv[] vetor com as palavras
 
+    char *palavra = NULL;
+    char *nome_arquivo = NULL;
     int n = 0;
     int i = 0;
-    int inicio = 1;
+    
 
-    for(int i = 1; i < argv[i]; i++){
-        if(strcmp(argv[i]) == "-n"){
+    for(int i = 1; i < argc; i++){
+        if(strcompr(argv[i]) == "-n"){
             n = 1;
-            inicio = i;
         }
-        if(strcomp(argv[i]) == "-i"){
+        else if(strcompr(argv[i]) == "-i"){
             i = 1;
-            inicio = i;
+        }
+        else if(palavra == NULL){
+            palavra = argv[i];
+        }
+        else if(nome_arquivo == NULL){
+            nome_arquivo = argv[i];
         }
     }
 
-    if(argc != 3 ){ // verifica se tem os 3 comandos digitados
-        printf("Uso: ./meugrep palavra arquivo\n");
+    if(palavra == NULL || nome_arquivo == NULL){ // verifica se o argumento vindo no main é valido 
+        printf("Uso: %s [opções] <palavra> <arquivo>\n", argv[0]);
         return 1;
     }
-
-    char *palavra = argv[1];
-    char *nome_arquivo = argv[2];
-
-    FILE *arquivo = fopen(nome_arquivo, "r");
+    
+    
+    FILE *arquivo = fopen(nome_arquivo, "r"); // fopen é uma função da biblioteca stdio.h que abre um arquivo e retorna um ponteiro para o arquivo. O primeiro argumento é o nome do arquivo e o segundo argumento é o modo de abertura (neste caso, "r" para leitura).
     if(arquivo == NULL){
         printf("Erro ao abrir o arquivo");
         return 1;
     }
 
     char linha[1000];
-    while(fgets(linha, sizeof(linha), arquivo))
-
-    if(strstr(linha, palavra)){
-        printf("%s", linha);
+    while(fgets(linha, sizeof(linha), arquivo)){ //faz cada linha do arquivo ler e gravar em linha
+        busca_palavra(palavra, linha);
     }
 
     fclose(arquivo);
