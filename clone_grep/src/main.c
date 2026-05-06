9ea776ab2929ed48f6dd43c99e05b2f1c91d68e0
Mateus Huster <mateushuster@gmail.com>
2026-05-06 18:48:41 +0000

    organizacao e producao do codigo do clone grep

--- clone_grep/src/main.c
+++ clone_grep/src/main.c
@@ -1,40 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <string.h>
+#include <ctype.h>
 
-void imprime_resultado(char *linha){
-    printf("%s", linha);
+typedef struct {
+    int numero_linha;
+    int ignorar_case;
+    int contar_ocorrencias;
+    int inverter_busca;
+} Opcoes;
+
+
+void minusculo(char *texto){
+    for(int i = 0; texto[i]; i++){
+        texto[i] = tolower(texto[i]);
+    }
 }
 
-void busca_palavra(char *palavra, char *linha){ //
-    return strstr(linha, palavra);// strstr é uma função da biblioteca string.h que procura a primeira ocorrência de uma substring (neste caso, a palavra) em uma string (neste caso, a linha). Se a substring for encontrada, a função retorna um ponteiro para a primeira ocorrência da substring na string. Caso contrário, retorna NULL.
+void busca (char *palavra, FILE *arquivo, Opcoes opcoes){ // adicionar o inverter_busca
+    char linha[1000];
+    int numero_linha = 1;
+    int ocorrencias = 0;
+
+    while(fgets(linha, sizeof(linha), arquivo)){
+        int encontrou = 0;
+
+        if(!opcoes.ignorar_case){
+            if(strstr(linha, palavra) != NULL){
+                encontrou = 1;
+            }
+        }else{
+            char copiaLinha[1000];
+            char copiaPalavra[100];
+
+            strcpy(copiaLinha, linha);
+            strcpy(copiaPalavra, palavra);
+
+            minusculo(copiaLinha);
+            minusculo(copiaPalavra);
+
+            if(strstr(copiaLinha, copiaPalavra) != NULL){
+                encontrou = 1;
+            }
+        }
+
+        if(encontrou){
+            if(opcoes.numero_linha){
+                printf("%d: %s", numero_linha, linha);
+            }
+            else if(opcoes.contar_ocorrencias){
+                ocorrencias++;
+            }
+            else{
+                printf("%s", linha);
+            }
+        }
+
+        numero_linha++;
+    }
 }
-
-void imprime_numeroLinha(int numero_linha, char *linha){
-    printf("%d: %s", numero_linha, linha);
-}
-
-
-
-
-
-
 
 
 int main(int argc, char *argv[]) { // argc numero de palavras escritas e *argv[] vetor com as palavras
 
     char *palavra = NULL;
     char *nome_arquivo = NULL;
-    int n = 0;
-    int i = 0;
+    Opcoes opcoes;
     
-
+    opcoes.numero_linha = 0;
+    opcoes.ignorar_case = 0;
+    opcoes.contar_ocorrencias = 0;
+    opcoes.inverter_busca = 0;
+    
     for(int i = 1; i < argc; i++){
-        if(strcompr(argv[i]) == "-n"){
-            n = 1;
+        if(strcmp(argv[i], "-n") == 0){
+            opcoes.numero_linha = 1;
         }
-        else if(strcompr(argv[i]) == "-i"){
-            i = 1;
+        else if(strcmp(argv[i], "-i") == 0){
+            opcoes.ignorar_case = 1;
+        }
+        else if(strcmp(argv[i], "-c") == 0){
+            opcoes.contar_ocorrencias = 1;
+        }
+        else if(strcmp(argv[i], "-v") == 0){
+            opcoes.inverter_busca = 1;
         }
         else if(palavra == NULL){
             palavra = argv[i];
@@ -49,17 +97,14 @@ int main(int argc, char *argv[]) { // argc numero de palavras escritas e *argv[]
         return 1;
     }
     
-    
     FILE *arquivo = fopen(nome_arquivo, "r"); // fopen é uma função da biblioteca stdio.h que abre um arquivo e retorna um ponteiro para o arquivo. O primeiro argumento é o nome do arquivo e o segundo argumento é o modo de abertura (neste caso, "r" para leitura).
     if(arquivo == NULL){
         printf("Erro ao abrir o arquivo");
         return 1;
     }
-
-    char linha[1000];
-    while(fgets(linha, sizeof(linha), arquivo)){ //faz cada linha do arquivo ler e gravar em linha
-        busca_palavra(palavra, linha);
-    }
+    
+    busca(palavra, arquivo, opcoes);
+    
 
     fclose(arquivo);
 
